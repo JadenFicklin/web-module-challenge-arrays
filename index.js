3d88109fbfd88e475f828a1b04b09d10ba7d2b80
Jaden Ficklin <86342418+JadenFicklin@users.noreply.github.com>
2021-08-11 15:32:21 -0600

    task 2

--- index.js
+++ index.js
@@ -60,8 +60,13 @@ Confirm that an array is exactly 31 flavors. Your function should accept:
 For Example: is31Flavors(originalFlavors) will return true if your code is working properly
 */
 
-function is31Flavors(/*your code here*/) {
-  /*your code here*/
+function is31Flavors(array) {
+  if (array.length === 31) {
+    return true;
+  } else {
+    return false;
+  }
+  console.log(is31Flavors(originalFlavors));
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
